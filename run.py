cb299cd2f036fc7b2dce31c302737b331323093e
Eyal Foni <eyalfoni@gmail.com>
2017-08-03 18:42:13 +0300

    two main pages

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flask import Flask, url_for, render_template
+from __future__ import print_function
+from flask import Flask, url_for, render_template, request
+import sys
 app = Flask(__name__)
 
 # This main page is a welcome page
@@ -13,4 +15,15 @@ def index():
 
 @app.route('/ttyl', methods=['GET', 'POST'])
 def main_page():
-    return 'fml'
+    if request.method == 'GET':
+        return render_template('main_page.html')
+    elif request.method == 'POST':
+        user_num = request.form['user_num']
+        sending_num = request.form['sending_num']
+        return user_num + ' ' + sending_num
+    else:
+        return 'What\'s going on?'
+
+
+def schedule_text():
+    pass
